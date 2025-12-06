5e95013b0b24960d137b47d22abf4eb97ad0f7c3
SMDpHeroB12 <smd.cloud2025@gmail.com>
2025-12-07 00:31:01 +0600

    SeatPao Initial Setup and gihub connected

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-7xl font-bold flex items-center justify-center h-screen w-11/12 mx-auto">
+        <h1 className="  ">Vite + React</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
